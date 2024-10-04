c1103f1df10f8b5029b34dc40101955f5f503247
ddppll <93955871+ddppll@users.noreply.github.com>
2024-10-04 14:37:50 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 9.88 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -9,6 +9,8 @@ class Solution {
                 if(Long.parseLong(a)<=Long.parseLong(p)) answer++;
             }
         }
+        
+        // 처음에 parseint 했을 때 런타임 에러 발생. p의 길이 제약조건이 18 이하임. int로 나타낼 수 있는 최대 자리수는 10자리이고 long은 19자리이기 때문에 long으로 변환해서 풀이해야 함
         return answer;
     }
 }
