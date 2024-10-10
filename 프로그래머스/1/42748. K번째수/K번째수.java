530cc924a3f75504fa4d44d2542266fb384a8270
ddppll <93955871+ddppll@users.noreply.github.com>
2024-10-10 22:59:38 +0900

    [level 1] Title: K번째수, Time: 0.92 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,11 +1,8 @@
 import java.util.Arrays;
 class Solution {
-    public int[] solution(int[] array, int[][] commands) {
+    public int[] solution(int[] array, int[][] commands) {        
         int[] answer = new int[commands.length];
-        int[] arr1 = Arrays.copyOfRange(array, commands[0][0]-1,commands[0][1]);
-
-
-       for(int i=0; i<commands.length; i++){
+        for(int i=0; i<commands.length; i++){
             int[] arr = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
             Arrays.sort(arr);
             answer[i] = arr[commands[i][2]-1];
